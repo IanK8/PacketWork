08b330f44d0bf9ad7ef98086ad37d38137b7d35a
IanKalotkin <114188361+IanK8@users.noreply.github.com>
2024-05-16 14:55:23 -0400

    s

--- Dice/Dice.java
+++ Dice/Dice.java
@@ -5,6 +5,13 @@ public class Dice {
     public Dice(){
         die1 = 1;
         die2 = 1;
-        
+    }
+    public void roll(){
+        die1 = ((int)(Math.random() * 6) + 1);
+        die2 = ((int)(Math.random() * 6) + 1);
+    }
+    public int getTotal(){
+        int sum = die1 + die2;
+        return sum;
     }
 }
