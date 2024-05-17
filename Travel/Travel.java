27234d7e7db8b200175c70795b28cd94a34f3f33
IanKalotkin <114188361+IanK8@users.noreply.github.com>
2024-05-17 14:46:27 -0400

    w

---
+++ Travel/Travel.java
@@ -0,0 +1,15 @@
+public class Travel{
+    private int people;
+    public Travel (int n){
+        people = n;
+    }
+    public int goByVan(){
+        n = 
+    }
+    public int goByCanoe(){
+
+    }
+    public int goByPlane(){
+
+    }
+}
